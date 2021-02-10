f6ea0ecc2ea2100cf764692842afe4ef26aae4cd
NightwolfCB <nightwolf63@rambler.ru>
2021-02-11 01:38:26 +0300

    Итерация 2

--- posts/views.py
+++ posts/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import PostForm
-from .models import Group, Post
+from .models import Group, Post, User
 
 
 def index(request):
@@ -36,23 +36,50 @@ def new_post(request):
 
 
 def profile(request, username):
-    author_posts = User.objects.get(username=username)
-    posts_count = Post.objects.count(username=username) # нам надо как-то передать количество постов автора
+    author = get_object_or_404(User, username=username)
+    posts_latest = author.posts.all().order_by('-pub_date')
+    paginator = Paginator(posts_latest, 10)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+    posts_count = Post.objects.filter(author=author).count
     context = {
-        "page": page,
-        "author_posts": author_posts
+        'page': page,
+        'posts_count': posts_count,
+        'posts_latest': posts_latest,
+        'author': author
     }
     return render(request, 'profile.html', context)
  
  
 def post_view(request, username, post_id):
-        # тут тело функции
-    return render(request, 'post.html', {})
+    user = get_object_or_404(User, username=username)
+    user_posts = Post.objects.filter(author=user)
+    total_posts = user_posts.count
+    post = user_posts.get_object_or_404(id=post_id)
+    context = {
+        'profile': user,
+        'post': post,
+        'total_posts': total_posts
+    }
+    return render(request, 'post.html', context)
+
 
 @login_required
 def post_edit(request, username, post_id):
-        # тут тело функции. Не забудьте проверить, 
-        # что текущий пользователь — это автор записи.
-        # В качестве шаблона страницы редактирования укажите шаблон создания новой записи
-        # который вы создали раньше (вы могли назвать шаблон иначе)
-    return render(request, 'post_new.html', {})
+    if request.user.username != username:
+        return redirect(reverse('post', username, post_id))
+    post = get_object_or404(Post, id=post_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse(post, username, post_id))
+        return redirect(reverse(post, username, post_id))
+        form_with_post = PostForm(instance=post)
+        if request.method == 'GET':
+            context = {
+                'form': form_with_post,
+                'edit': True,
+                'post_id': post.id
+            }
+            return render(request, 'post_edit.html', context)
