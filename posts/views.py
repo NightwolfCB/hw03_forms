b89b2795dd60e2481a272027a7f2f4eae19b7060
NightwolfCB <nightwolf63@rambler.ru>
2021-02-12 17:40:33 +0300

    Итерация 4

--- posts/views.py
+++ posts/views.py
@@ -41,10 +41,8 @@ def profile(request, username):
     paginator = Paginator(posts_latest, 10)
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
-    posts_count = Post.objects.filter(author=author).count
     context = {
         'page': page,
-        'posts_count': posts_count,
         'posts_latest': posts_latest,
         'author': author
     }
@@ -52,37 +50,13 @@ def profile(request, username):
  
  
 def post_view(request, username, post_id):
-    user = get_object_or_404(User, username=username)
-    user_posts = Post.objects.filter(author=user)
-    total_posts = user_posts.count
-    post = user_posts.get_object_or_404(id=post_id)
+    author = get_object_or_404(User, username=username)
+    post = get_object_or_404(Post, id=post_id, author=author)
     context = {
-        'profile': user,
-        'post': post,
-        'total_posts': total_posts
+        'author': author,
+        'post': post        
     }
     return render(request, 'post.html', context)
-
-
-# @login_required
-# def post_edit(request, username, post_id):
-#     if request.user.username != username:
-#         return redirect(reverse('post', username, post_id))
-#     post = get_object_or404(Post, id=post_id)
-#     if request.method == 'POST':
-#         form = PostForm(request.POST, instance=post)
-#         if form.is_valid():
-#             form.save()
-#             return redirect(reverse(post, username, post_id))
-#         return redirect(reverse(post, username, post_id))
-#         form_with_post = PostForm(instance=post)
-#         if request.method == 'GET':
-#             context = {
-#                 'form': form_with_post,
-#                 'edit': True,
-#                 'post_id': post.id
-#             }
-#             return render(request, 'post_edit.html', context)
 
 
 @login_required
